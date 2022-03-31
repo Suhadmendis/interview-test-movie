b9ec361942acb835c919b8b602a3d4958f970d23
Akila Mendis <akila@stacktech.io>
2022-03-31 14:23:44 +0530

    close button function

--- _js/script_home.js
+++ _js/script_home.js
@@ -15,31 +15,19 @@ new Vue({
                         const description = movie.show.summary;
                         // const image = movie.show.image.original;
                         if (movie.show.image?.original != undefined) {
+                            const unique = Math. floor(Math. random() * 10000000);
                             const image = movie.show.image.original;
-                            movies.push({ title, description, image });
+                            movies.push({ id: unique, title, description, image });
                         }
-
                         
                     });  
                     
-                    this.MOVIES = [];
-
-                    
-                        this.MOVIES = movies;
-                    
-                    
-
+                    this.MOVIES = [];                    
+                    this.MOVIES = movies;
                 }else{
-                    
                     alert("No results found");
-                    
                     this.initialData();
-
                 }
-                
-
-                
-                
             });
         }
     },
@@ -48,24 +36,36 @@ new Vue({
     },
     methods: {
         initialData: function () {
+
+
+            
+            // console.log(unique);
+
             const initial_data = [
                 {
+                    id: Math. floor(Math. random() * 10000000),
                     title: "Batman Returns",
                     description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut...",
                     image: "_img/movie_image/Batman.jpg"
                 },
                 {
+                    id: Math. floor(Math. random() * 10000000),
                     title: "Wild Wild West",
                     description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut...",
                     image: "_img/movie_image/Wild West.jpg"
                 },
                 {
+                    id: Math. floor(Math. random() * 10000000),
                     title: "The Amazing Spiderman",
                     description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut...",
                     image: "_img/movie_image/Spiderman.jpg"
                 }
             ];
             this.MOVIES = initial_data;
+        },
+        closeMovie: function (targetMovie) {
+            
+            this.MOVIES = this.MOVIES.filter(movie => movie.id != targetMovie.id);
         },
         truncate: function (text, length, suffix) {
             if (text.length > length) {
